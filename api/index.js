a686a6b5c1c31330a9f62bad9132d46053e69c4c
naddamuhamed <79428643+naddamuhamed@users.noreply.github.com>
2023-06-23 20:53:44 +0300

    post and patch api

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 const app=require("express")()
+const bodyparser=require("body-parser")
+app.use(bodyparser.json())
+app.use(
+    bodyparser.urlencoded({
+        extended: true,
+    })
+)
 
 app.listen(3000,()=>console.log("connected to server"))
 app.get("/",(req,res)=>{
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -3,4 +3,23 @@ const router= require("express").Router()
 router.get("/ping",(req,res)=>{
     res.json({response: "pong"})
 })
+router.post("/pong",(req,res)=>{
+    const request=req.body
+    // res.json(request)
+    if(request.ping){
+        res.json(request)
+    }
+    else{
+        res.status(400).json({error:"not allowed"})
+    }
+})
+router.patch("/pingpong",(req,res)=>{
+    const request=req.body
+    if(request._id == "ping"){
+        res.json({"response":"pong"})
+    }
+    else{
+        res.status(400).json({"error":"not allowed"})
+    }
+})
 module.exports=router
