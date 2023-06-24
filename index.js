bbef6f31db46087e0f14168d2d8559829bd99f51
naddamuhamed <79428643+naddamuhamed@users.noreply.github.com>
2023-06-24 21:42:21 +0300

    rendering  html  and sending it to server

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express=require("express")
+const path=require('path')
+const hoganmiddleware=require("hogan-middleware")
 const app=express()
-
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','mustache')
+app.engine('mustache',hoganmiddleware.__express)
 app.get('/',(req,res,next)=>{
 res.send("hello!")
 })
+app.get('/json',(req,res,next)=>{
+    const data=({greeting:"hello"})
+    res.json(data)
+    })
 
+
+    app.get('/home',(req,res,next)=>{
+        res.render('home',null)
+    })
 app.listen(3000) //3000.5000.8000.8080
